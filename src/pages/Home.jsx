91d73475ebc3334df29d90bc9f62c5cd9ac019b2
SeryVathana <yooseryvathana@gmail.com>
2023-12-08 15:22:46 +0700

    Add Sign In in Home for samnang

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import { AspectRatio, Button, Grid, Stack, Typography } from '@mui/joy';
 import { FaArrowRightLong } from 'react-icons/fa6';
 import ItemCard from '../components/ItemCard';
+import SignIn from './SignIn';
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      <SignIn />
+      {/* <Hero />
       <Categories />
       <CardContainer />
       <CardContainer />
-      <CardContainer />
+      <CardContainer /> */}
     </>
   );
 }
